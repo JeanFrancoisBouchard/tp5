243640588fa984b741bd3b768a50f3c73a59511d
Jean-Francois Bouchard <jeanfrancoisbouchard3@gmail.com>
2019-03-04 16:22:53 -0500

    should be ok

---
+++ tp5/Controllers/UserController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using tp5.Services;
+using tp5.Models;
+
+namespace tp5.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserService _userService;
+
+        public UserController(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("create")]
+        public ActionResult create([FromBody]User user)
+        {
+            if(_userService.create(user))
+            {
+                return StatusCode(200);
+            }
+            else
+            {
+                return StatusCode(409);
+            }
+        }
+
+        [HttpPost("login")]
+        public ActionResult login([FromBody]User user)
+        {
+            if(_userService.login(user))
+            {
+                string token = Convert.ToBase64String(Guid.NewGuid().ToByteArray());
+                HttpContext.Session.SetString("token", token);
+
+                return StatusCode(200);
+            }
+            else
+            {
+                return StatusCode(401);
+            }
+        }
+
+        [HttpPost("logout")]
+        public ActionResult logout()
+        {
+            HttpContext.Session.Clear();
+
+            return StatusCode(200);
+        }
+
+        [HttpGet("secret")]
+        public ActionResult<string> secret([FromBody]dynamic token)
+        {
+            if (token != null && token == HttpContext.Session.GetString("token"))
+            {
+                return "check son pouce, y curve";
+            }
+            else
+            {
+                return StatusCode(403);
+            }
+        }
+    }
+}
---
+++ tp5/Services/UserService.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+using MongoDB.Driver;
+using tp5.Models;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+
+namespace tp5.Services
+{
+    public class UserService
+    {
+        private readonly IMongoCollection<User> _Users;
+        private readonly byte[] _Salt = new byte[128 / 8];
+
+        public UserService(IConfiguration config)
+        {
+            var dbClient = new MongoClient(config.GetConnectionString("db"));
+            var db = dbClient.GetDatabase("tp5");
+            _Users = db.GetCollection<User>("users");
+            
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(_Salt);
+            }
+        }
+
+        public bool create(User user)
+        {
+            if(!_Users.Find(tempuser => tempuser.username.ToLower() == user.username.ToLower()).Any())
+            {
+                _Users.InsertOne(user);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool login(User user)
+        {
+            user.password = encodePassword(user.password);
+
+            if (_Users.Find(tempuser => tempuser.username.ToLower() == user.username.ToLower() && tempuser.password == user.password).Any())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public string encodePassword(string password)
+        {
+            string encoded = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: _Salt,
+                prf: KeyDerivationPrf.HMACSHA1,
+                iterationCount: 10000,
+                numBytesRequested: 256 / 8));
+
+            return encoded;
+        }
+    }
+}
